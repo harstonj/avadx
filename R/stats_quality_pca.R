7633c77630ed7e409b22813e6190986faadfd985
Max Miller <mmiller@bromberglab.org>
2020-06-08 09:05:45 -0400

    added cmdline args amd fixed bug in stats_quality_pca.R

--- R/stats_quality_pca.R
+++ R/stats_quality_pca.R
@@ -6,7 +6,9 @@ library(ggfortify, quietly=T, warn.conflicts=F)
 
 option_list = list(
   make_option(c("-f", "--stats"), type="character", default=NULL, 
-              help="the path to the stats file output from bcftools", metavar="character")
+              help="the path to the stats file output from bcftools", metavar="character"),
+  make_option(c("-o", "--output"), type="character", default=NULL, 
+              help="path for output pdf", metavar="character")
 )
 
 opt = parse_args(OptionParser(option_list=option_list))
@@ -17,11 +19,12 @@ system(paste0("grep PSC ", f," > ", f, ".PSC.txt"))
 df <- fread(paste0(f, ".PSC.txt"), skip=1, data.table=F)
 
 rownames(df) <- df$`[3]sample`
-pca <- prcomp(df[, c(4:8,10,11,14)], scale.=T)
+df_ = df[, c(4:8,10,11,14)]
+df_ = df_[ , which(apply(df_, 2, var) != 0)]
+pca <- prcomp(df_, scale.=T)
 
 p <- autoplot(pca, label=T, label.size=4, loadings=T, loadings.label=T, label.vjust=1.5, loadings.label.vjust=1.5)
 
-ggsave(filename=paste0(basename(f), ".PSC.PCA.pdf"),
+ggsave(filename=opt$output,
 	   plot=p,
-	   path=dirname(f),
 	   width=9, height=9)

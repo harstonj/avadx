bd394209ec42b6fdb4100e8f1e339bb944dd3b70
Max Miller <mmiller@bromberglab.org>
2020-06-18 19:52:56 -0400

    updated check_missing_SNAP.R

--- R/check_missing_SNAP.R
+++ R/check_missing_SNAP.R
@@ -10,7 +10,7 @@ outputfolder <- args[3]
 
 setwd(dbfolder)
 df_len <- read.csv("Transcript-ProtLength.csv", stringsAsFactors=F, header=T)
-# mutOut <- read.table("Mutations.mutOut", stringsAsFactors=F, header=F, sep="\t")
+#mutOut <- read.table("Mutations.mutOut", stringsAsFactors=F, header=F, sep="\t")
 
 exonic <- data.table::fread(exonic.file, header=F, data.table=F)
 exonic <- exonic[exonic$V3!="UNKNOWN",]
@@ -41,15 +41,17 @@ for(i in 1:length(variants)){
 
 gene_mRNA <- data.table::rbindlist(gene_mRNA_list, use.names=T)
 gene_mRNA$prot_length <- df_len$Prot_length[match(gene_mRNA$Transcript, df_len$Transcript)]
-if(any(is.na(gene_mRNA$prot_length))){
-  setwd(outputfolder)
+
+setwd(outputfolder)
   write.table(unique(as.character(gene_mRNA$Transcript[is.na(gene_mRNA$prot_length)])), 
-              file="missing_mRNA_accession.txt", quote=F, col.names=F, row.names=F)
+              file="missing_mRNA_accession_global.txt", quote=F, col.names=F, row.names=F)
+if(any(is.na(gene_mRNA$prot_length))){
   print(
-    paste0("Transcript-ProtLength.csv lacks protein length info for ",
-    length(which(is.na(gene_mRNA$prot_length))), " instances (",
-    round(length(which(is.na(gene_mRNA$prot_length)))/nrow(gene_mRNA)*100), "%). "
-    ,"Please check and run this script again.")
+    paste0(
+      "Transcript-ProtLength.csv lacks protein length info for ",
+      length(which(is.na(gene_mRNA$prot_length))), " GLOBAL transcripts (",
+      round(length(which(is.na(gene_mRNA$prot_length)))/nrow(gene_mRNA)*100), "%). "
+    )
   )
 }
 
@@ -64,6 +66,19 @@ gene_mRNA_nodup <- gene_mRNA %>%
   group_by(line) %>%
   filter(prot_length == max(prot_length))
 
+setwd(outputfolder)
+write.table(unique(as.character(gene_mRNA_nodup$Transcript[is.na(gene_mRNA_nodup$prot_length)])), 
+            file="missing_mRNA_accession_query.txt", quote=F, col.names=F, row.names=F)
+if(any(is.na(gene_mRNA_nodup$prot_length))){
+  print(
+    paste0(
+      "Transcript-ProtLength.csv lacks protein length info for ",
+      length(which(is.na(gene_mRNA_nodup$prot_length))), " QUERY transcripts (",
+      round(length(which(is.na(gene_mRNA_nodup$prot_length)))/nrow(gene_mRNA_nodup)*100), "%)"
+    )
+  )
+}
+
 # Read in fasta files:
 setwd(dbfolder)
 fas <- read.fasta("prot_seqs.txt", as.string=T, seqtype="AA", set.attributes=F)
@@ -76,25 +91,41 @@ missing_prot_seq <- setdiff(unique(as.character(gene_mRNA$Transcript)), fas_mRNA
 
 if(length(missing_prot_seq)>0){
   setwd(outputfolder)
-  write.table(missing_prot_seq, "TranscriptAccess_missing_prot_seq.txt", quote=F, col.names=F, row.names=F)
-  print(paste0(length(missing_prot_seq), " transcripts do not have protein sequences in the db folder. Please append the protein sequence into the prot_seqs.txt file! mRNA transcript accession numbers were output to the output folder and is named: TranscriptAccess_missing_prot_seq.txt. Please use NCBI batch entrez query to obtain protein sequences (https://www.ncbi.nlm.nih.gov/sites/batchentrez)."))
+  write.table(missing_prot_seq, "TranscriptAccess_missing_prot_seq_global.txt", quote=F, col.names=F, row.names=F)
+  print(paste0(length(missing_prot_seq), " GLOBAL transcripts do not have protein sequences in the db folder. Please append the protein sequence into the prot_seqs.txt file! mRNA transcript accession numbers were output to the output folder and is named: TranscriptAccess_missing_prot_seq.txt. Please use NCBI batch entrez query to obtain protein sequences (https://www.ncbi.nlm.nih.gov/sites/batchentrez)."))
 }
 
-# 2. make SNAP input:
-setwd(outputfolder)
-for(i in 1:nrow(gene_mRNA_nodup)){
-  aaMutation <- as.character(gene_mRNA_nodup$aaMut)[i]
-  pos <- as.numeric(substr(aaMutation, 2, nchar(aaMutation)-1))
-  ref <- substr(aaMutation, 1, 1)
-  alt <- substr(aaMutation, nchar(aaMutation), nchar(aaMutation))
-  trans <- as.character(gene_mRNA_nodup$Transcript)[i]
+# # 2. make SNAP input:
+# setwd(outputfolder)
+# for(i in 1:nrow(gene_mRNA_nodup)){
+#   aaMutation <- as.character(gene_mRNA_nodup$aaMut)[i]
+#   pos <- as.numeric(substr(aaMutation, 2, nchar(aaMutation)-1))
+#   ref <- substr(aaMutation, 1, 1)
+#   alt <- substr(aaMutation, nchar(aaMutation), nchar(aaMutation))
+#   trans <- as.character(gene_mRNA_nodup$Transcript)[i]
   
-  aaSeq <- fas[[match(trans, fas_mRNA)]]
-  # check if REF is in line with aaSeq: otherwise ignore this mutation
-  if(ref==substr(aaSeq, pos, pos) & alt!="X"){
-    write.table(aaMutation, paste0(trans, ".mutation"), quote=F, row.names=F, col.names=F, append=T)
-  }
-  if(!file.exists(paste0(trans, ".fasta"))){
-    write.fasta(aaSeq, fas_names[match(trans, fas_mRNA)], file.out=paste0(trans, ".fasta"))
-  }
+#   aaSeq <- fas[[match(trans, fas_mRNA)]]
+#   # check if REF is in line with aaSeq: otherwise ignore this mutation
+#   if(ref==substr(aaSeq, pos, pos) & alt!="X"){
+#     write.table(aaMutation, paste0(trans, ".mutation"), quote=F, row.names=F, col.names=F, append=T)
+#   }
+#   if(!file.exists(paste0(trans, ".fasta"))){
+#     write.fasta(aaSeq, fas_names[match(trans, fas_mRNA)], file.out=paste0(trans, ".fasta"))
+#   }
+# }
+
+# 1. check if prot_seq.txt contains all sequence for relevant query subset:
+missing_prot_seq_nodup <- setdiff(unique(as.character(gene_mRNA_nodup$Transcript)), fas_mRNA)
+setwd(outputfolder)
+  write.table(missing_prot_seq_nodup, "TranscriptAccess_missing_prot_seq_query.txt", quote=F, col.names=F, row.names=F)
+
+if(length(missing_prot_seq_nodup)>0){
+  print(paste0(length(missing_prot_seq_nodup), " QUERY transcripts do not have protein sequences in the db folder"))
 }
+
+# 2. Generate snapcachedb input
+setwd(outputfolder)
+matched = match(unique(gene_mRNA_nodup$Transcript), fas_mRNA)
+write.fasta(fas[matched], fas_mRNA[matched], file.out='snapfun_query.fa')
+write.table(gene_mRNA_nodup[c('Transcript', 'aaMut')], "snapfun_query.ids", quote=F, col.names=F, row.names=F)
+write.table(gene_mRNA_nodup, "snapfun_gene_mRNA_nodup.tsv", quote=F, col.names=T, row.names=T)

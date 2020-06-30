bdd6c9a846c52b8c641355e34d7d234ab16334d9
Max Miller <mmiller@bromberglab.org>
2020-06-29 22:05:37 -0400

    pushed python requirement to ≥ 3.8

--- build/app/__main__.py
+++ build/app/__main__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-if sys.version_info[0] == 3 and sys.version_info[1] >= 6:
+if sys.version_info[0] == 3 and sys.version_info[1] >= 8:
     pass
 else:
-    raise Exception("mi-faser requires Python >= 3.6. This version is: %s" % ".".join('%s' % _ for _ in sys.version_info[:3]))
+    raise Exception("mi-faser requires Python >= 3.8. This version is: %s" % ".".join('%s' % _ for _ in sys.version_info[:3]))
 
 
 def main():

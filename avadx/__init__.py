f5303f7f76e46e357833f3573ec447ab3e460de3
Max Miller <mmiller@bromberglab.org>
2020-12-15 18:29:15 -0500

    bumped version to 2.02

--- avadx/__init__.py
+++ avadx/__init__.py
@@ -1,7 +1,7 @@
 name = "avadx"
 __author__ = 'mmiller'
-__version__ = '2.01'
-__releasedate__ = '12/08/20'
+__version__ = '2.02'
+__releasedate__ = '12/15/20'
 __all__ = [
     'pipeline',
     'helper',
